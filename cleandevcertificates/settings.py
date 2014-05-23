fe455509e581b3aead4731d153be436b49eecdf3
matheus <oliveira.matheusde@gmail.com>
2014-05-22 23:08:17 -0300

    separate apps

--- cleandevcertificates/settings.py
+++ cleandevcertificates/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'south',
+
     'cleandevcertificates.core',
     'cleandevcertificates.events',
 )
